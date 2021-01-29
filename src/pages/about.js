066c11672102af817fb8581aa4ab388fb6e1c074
Leo Spallino <spallino@icloud.com>
2021-01-28 21:16:03 -0500

    Add content to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,9 +17,31 @@ const About = () => {
           University expecting to graduate in April 2022.
           <br />
           <br />
+          I’m extremely passionate about web development and designing creative
+          solutions to everyday problems. Outside of the web, I’m interested in
+          camping, playing boardgames with friends, going to concerts, and
+          playing music.
+          <br />
+          <br />
           I'm currently seeking co-op opportunities beginning in May 2021, and
           have experience working with React, React-Native, JavaScript, HTML,
-          CSS
+          CSS.
+          <br />
+          <br />
+          Technologies I use:
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>React / React-Native</li>
+            <li>Git</li>
+            <li>Node.js</li>
+            <li>Python</li>
+            <li>PL/SQL</li>
+            <li>Oracle APEX</li>
+            <li>Gatsby.js</li>
+            <li>Firebase</li>
+          </ul>
         </p>
       </div>
     </Layout>
